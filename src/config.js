0cd3133a88dcc943b29276de6c73f842f0bae617
Kevin Jin <kjin@Kevins-MacBook-Pro-2.local>
2017-08-01 19:21:01 +0200

    config

---
+++ src/config.js
@@ -0,0 +1,18 @@
+var config = {};
+
+config.bots = [
+  {
+    botID: 'a74d68e3871e21f968d7e8ea6a',
+    groupID: '30807237',
+    groupLocalID: '1', // an identifier for this group used in this application only
+    botName: 'arthur',
+    groupName: 'Evil?' // optional
+  }
+];
+
+config.yahooWeatherAppId = '';
+
+// Travis CI user token
+config.travisUserToken = 'xxxxxxxxxxxxxx';
+
+module.exports = config;
